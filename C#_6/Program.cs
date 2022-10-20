a478d16b9217bbe8d7c22beca46781491cec1278
Helen <helen@Mac.local>
2022-10-20 19:46:46 +0700

    Task massiv 9999

---
+++ C#_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа. 
+int num = new Random().Next(10, 9999);
+int max = num % 10;
+
+Console.WriteLine(num);
+
+while (num != 0)
+{
+    int k = num % 10;
+    if (k > max) max = k;
+    num = num / 10;
+}
+Console.WriteLine(max);
