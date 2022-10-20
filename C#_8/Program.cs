3d4adeba19b8e7fb4da0829f504f13eb7c3a4505
Helen <helen@Mac.local>
2022-10-20 21:03:32 +0700

    Task 8

---
+++ C#_8/Program.cs
@@ -0,0 +1,10 @@
+﻿// 
+Console.Write("Введите 1 число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 % num2 == 0)
+Console.WriteLine("Кратно");
+
+else Console.WriteLine($"Не кратно, остаток от деления равен {num1 % num2}");
