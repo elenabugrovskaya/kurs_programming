9266afb55b1a5ce645b9ad76e2e550c47d79ed23
Helen <helen@Mac.local>
2022-10-20 20:23:04 +0700

    Task massiv 3

---
+++ C#_7/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа. 
+
+int num = new Random().Next(10, 999);
+
+Console.WriteLine(num);
+
+int result = (num / 100) * 10 + num % 10;
+
+Console.WriteLine(result);
