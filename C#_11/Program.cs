6c8db9f133eeb57f7b8a167ea28d3db94d3fcc3c
Helen <helen@Mac.local>
2022-10-23 14:58:32 +0700

    C#_10

---
+++ C#_11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите програрамму, которая по заданному номеру четверти, показывает возможный диапазон координат точек в этой четверти (x, y).
+
+void diapason (int a)
+{
+    if (a == 1) Console.WriteLine("x > 0 и y > 0");
+    else if (a == 2) Console.WriteLine("x < 0 и y > 0");
+    else if (a == 3) Console.WriteLine("x < 0 и y < 0");
+    else Console.WriteLine("x > 0 и y < 0");
+}
+int a = int.Parse(Console.ReadLine());
+diapason (a);
