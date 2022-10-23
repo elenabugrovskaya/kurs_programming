c3d21121fd763c5a52f29e6a7ccf304518c889a3
Helen <helen@Mac.local>
2022-10-23 17:46:44 +0700

    C#_12

---
+++ C#_12/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход координаты 2 точек и находит  расстояние м/д ними в пространстве.
+
+
+double dist (int x1, int y1, int x2, int y2)
+{
+    double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    return res;
+}
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+
+double otvet = dist(x1,y1,x2,y2);
+Console.WriteLine(otvet);
