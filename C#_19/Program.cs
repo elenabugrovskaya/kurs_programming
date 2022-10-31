3a1637b3cb347effd93cdfe96643e68e9e2c3665
Helen <helen@Mac.local>
2022-10-31 18:06:59 +0700

    C#_19

---
+++ C#_19/Program.cs
@@ -0,0 +1,28 @@
+﻿// SЗадача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные,
+// и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] CreateMas (int n)
+{
+    int[] arr = new int[n];
+    for (int i=0; i<n; i++)
+    arr[i] = new Random().Next(2, 10);
+    return arr; 
+} 
+void Print(int[] arr)
+{
+    Console.WriteLine(String.Join(" ", arr));
+    Console.Write("\n");
+}
+int[] Zamena (int[] arr)
+{
+    for (int i=0; i<10; i++)
+        arr[i] = -arr[i];
+   return arr;
+}
+int[] array = new int[10];
+array = CreateMas(10);
+Print(array);
+Zamena(array);
+Print(array);
+
