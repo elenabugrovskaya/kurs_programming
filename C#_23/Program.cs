7f47d4a3c7d0ca0f8bbc4f25154f42fab4a036b0
Helen <helen@Mac.local>
2022-11-02 14:42:18 +0700

    C#_23

---
+++ C#_23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой 
+// длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a+b>c && a+c>b && b+c>a)
+Console.WriteLine("Да");
+else Console.WriteLine("Нет");
---
+++ С#_24/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Не используя рекурсию, выведите первые N чисел Фибоначчи: 0 и 1.
+
+int n = int.Parse(Console.ReadLine());
+int first = 0;
+int second = 1;
+Console.Write(first + " " + second + " ");
+for (int i = 3; i <= n; i++)
+{
+    int next = first + second;
+    Console.Write(next + " ");
+    first = second;
+    second = next;
+}
