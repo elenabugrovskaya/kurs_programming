5fcdb8b996904a4f5c6caa1f0d6ad82eb7007d4a
Helen <helen@Mac.local>
2022-11-02 13:21:16 +0700

    C#_22

--- С#_22/Program.cs
+++ С#_22/Program.cs
@@ -1,5 +1,22 @@
 ﻿// Перевести десятичное число в двоичное
 
 int number = int.Parse(Console.ReadLine());
-string number_2 = Convert.ToString(number, 2);
+int osn = 16;
+string number_2 = Convert.ToString(number, 16); //2, 8, 16, 32
+
 Console.WriteLine(number_2);
+Console.WriteLine(perevod(number, osn));
+
+string perevod (int number, int osn)
+{
+    string res = " ";
+    string nums = "0123456789ABCDEF";
+    while (number > 0)
+    {
+        //int del = number/osn;
+        //res = nums[number - del*osn] + res;
+        res = nums[number%osn]+res;
+        number /= osn;
+    }
+    return res;
+}
