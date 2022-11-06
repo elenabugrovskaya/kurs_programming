36ba916b580fe5d494e282ccc26825b0d3e07b9e
Helen <helen@Mac.local>
2022-11-06 15:49:14 +0700

    C#_27

---
+++ C#_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте двумерный массив размером  m x n, заполненный случайными дробными числами.
+
+void FillList(List<List<double>> mylist)
+{
+    for(int i=0; i<5; i++)
+    {
+        mylist.Add(new List<double>());
+        for(int j=0; j<5; j++)
+        mylist[i].Add(new Random().NextDouble()*5);
+    }
+}
+void PrintList(List<List<double>> mylist)
+{
+    for(int i=0; i<mylist.Count; i++)
+    {
+        for(int j=0; j<mylist.Count; j++)
+        Console.Write(mylist[i][j] + " ");
+        Console.WriteLine();
+    }
+}
+List<List<double>> numbers = new List<List<double>>();
+FillList(numbers);
+PrintList(numbers); 
