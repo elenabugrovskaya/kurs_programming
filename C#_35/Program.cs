bb5e96ec3653d36d8e176684b10cb58deb6c1e97
Helen <helen@Mac.local>
2022-11-14 11:32:33 +0700

    C#_33

---
+++ C#_35/Program.cs
@@ -0,0 +1,96 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз 
+// встречается элемент входных данных.
+// 1 2 3    1 - встречается 2 раза 
+// 4 5 6    2 - встречается 1 раз и т.д.
+// 7 1 9
+
+void FillMas (int[,] arr)
+{
+    for (int i=0; i<5;i++)
+    for (int j=0; j<5; j++)
+    arr[i, j] = new Random().Next(2, 10);
+}
+void Output (int [,] arr)
+{
+    for (int i=0; i<arr.GetLength(0); i++)
+    {
+        for (int j=0; j<arr.GetLength(1); j++)
+        Console.Write(arr[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int [,] Swap (int [,] arr)
+{
+    int LastIndex = arr.GetLength(0) - 1;
+    for (int j=0; j<arr.GetLength(1); j++)
+    {
+        int temp = arr[0, j];
+        arr[0, j] = arr[LastIndex, j];
+        arr[LastIndex, j] = temp; 
+    }
+    return arr;
+}
+int[] GetArray (int [,] array)
+{
+    int[] ArrayOne = new int[array.GetLength(0)*array.GetLength(1)];
+    int k = 0;
+     for (int i=0; i<array.GetLength(0); i++)
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            ArrayOne[k] = array[i, j];
+            k++;
+        }
+        PrintMasOne(ArrayOne);
+        return ArrayOne;
+}
+void PrintMasOne(int[] array)
+{
+    foreach (var el in array)
+        {
+          Console.Write(el + " ");  
+        }
+        Console.WriteLine();  
+}
+void SortMas (int[] array)
+{ 
+ for (int i=0; i<array.Length-1; i++)
+ {
+        for (int j=i+1; j<array.Length; j++)  
+        if(array[i]>array[j])
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        } 
+ }
+        PrintMasOne(array);
+ 
+}
+
+void FillDictionary(int[,] array)
+{
+    Dictionary<int, int> numbers = new Dictionary<int, int>();
+    for (int i=0; i<array.GetLength(0); i++)
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            if(numbers.ContainsKey(array[i, j]))
+            numbers[array[i,j]] += 1;
+            else
+            numbers.Add(array[i, j], 1);
+        }    
+    foreach (var num in numbers)
+        {
+          Console.WriteLine($"Число {num.Key}  встретилось {num.Value} ");  
+        }
+        
+}
+int [,] array = new int [5, 5];
+FillMas(array);
+int [,] newArr = new int [5, 5];
+newArr = Swap(array);
+Output(newArr);
+//int[] arr1 = GetArray(newArr);
+//SortMas(arr1);
+FillDictionary(newArr);
+
---
+++ С#_34/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа
+// должна вывести сообщение для пользователя.
+
+void FillMas (int[,] arr)
+{
+    for (int i=0; i<5;i++)
+    for (int j=0; j<5; j++)
+    arr[i, j] = new Random().Next(2, 10);
+}
+void Output (int [,] arr)
+{
+    for (int i=0; i<arr.GetLength(0); i++)
+    {
+        for (int j=0; j<arr.GetLength(1); j++)
+        Console.Write(arr[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int [,] FullSwap (int [,] arr)
+{
+    int[,] rArr = new int [arr.GetLength(1), arr.GetLength(0)];
+    for (int i=0; i<arr.GetLength(0); i++)
+    for (int j=0; j<arr.GetLength(1); j++)
+    rArr[j, i] = arr[i, j];
+    //Output(rArr);
+    return rArr;
+}
+
+int [,] array = new int [5, 5];
+FillMas(array);
+Output(array);
+int [,] ReversArr = new int [5, 5];
+ReversArr = FullSwap(array);
+//newArr = Swap(array);
+Output(ReversArr);
+
