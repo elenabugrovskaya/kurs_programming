f6e43e95412655d18134d9ee3ce9eb9faf55d104
Helen <helen@Mac.local>
2022-11-16 20:14:10 +0700

    C#_38

---
+++ C#_38/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int  SumNumbers (int n)
+{
+    if (n%10 == 0) return n;
+    else 
+        return (n%10 + SumNumbers (n/10));
+}
+Console.WriteLine(SumNumbers(53271));
---
+++ C#_39/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string FillNumbers (int n)
+{
+    if (n == 1) return (n).ToString();
+    else 
+        return (n + " " + FillNumbers (n-1));//m + " " + FillNumbers(m+1, n));
+}
+Console.WriteLine(FillNumbers(5));
