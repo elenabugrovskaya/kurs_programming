4da879213baef7f2db766b61cc8c2e1fa9340222
Helen <helen@Mac.local>
2022-11-16 17:12:02 +0700

    C#_37

---
+++ C#_37/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значения M и N. Напишите программу, которая выдедет все натуральные числа в промежутке от M до N.
+// M =2, N =5 -> "2, 3, 4, 5"
+
+string FillNumbers (int m, int n)
+{
+    if (m == n) return (m).ToString();
+    else 
+        return (m + " " + FillNumbers(m+1, n));
+}
+Console.WriteLine(FillNumbers(2, 8));
+
+
