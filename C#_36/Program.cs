905d4e68944ec51078744c67fb715da1783178ad
Helen <helen@Mac.local>
2022-11-16 16:43:04 +0700

    C#_36

---
+++ C#_36/Program.cs
@@ -0,0 +1,14 @@
+﻿// Залайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N. С помощью рекурсии.
+// n = 5 -> "1, 2, 3, 4, 5"
+
+int k = 0;
+string FillNumbers (int n)
+{
+    if (n == 1) return (k+1).ToString();
+    else 
+    {
+        k++; 
+        return (k + " " + FillNumbers(n-1));
+    }
+}
+Console.WriteLine(FillNumbers(5));
